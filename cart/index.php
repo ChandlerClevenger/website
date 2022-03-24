48502cc9065ba29ea54a01207ea05198780b9dc2
Chandler Clevenger <Chandler888@icloud.com>
2022-03-23 21:53:24 -0400

    better mysql queries to update cart

--- cart/index.php
+++ cart/index.php
@@ -2,17 +2,26 @@
 session_start();
 include("../connection.php");
 $user = $_SESSION['user'];
-
 if ( isset($_POST['quantity']) && isset($_POST['id']) ) {
     $quantity = $_POST['quantity'];
     $itemId = $_POST['id'];
-    $query = "INSERT INTO CART VALUES (:u, :ISBN, :q)";
+    $query = "SELECT COUNT(*) FROM CART WHERE Username=:u AND ISBN=:ISBN" ;
     $sql = $conn->prepare($query);
-    $sql->execute(["u"=>$user, "ISBN"=>$itemId, "q"=>$quantity]);
-    $_SESSION['cartSubmitted'] = true;
+    $sql->execute(["u"=>$user, "ISBN"=>$itemId]);
+    $alreadyIn = $sql->fetch()[0];
+ 
+    if($alreadyIn) {
+        $query = "UPDATE CART SET Quantity = Quantity + :q WHERE Username=:u AND ISBN=:ISBN";
+        $sql = $conn->prepare($query);
+        $sql->execute(["u"=>$user, "ISBN"=>$itemId, "q"=>$quantity]);
+    } else {
+        $query = "INSERT INTO CART VALUES (:u, :ISBN, :q)";
+        $sql = $conn->prepare($query);
+        $sql->execute(["u"=>$user, "ISBN"=>$itemId, "q"=>$quantity]);
+    }
     unset($_POST['quantity']);
     unset($_POST['id']);
-    header('Location: ../cart');
+    header("Location: ../cart");
 }
 
 $query = "SELECT Username, ISBN, SUM(Quantity) AS Total FROM CART WHERE Username=:u GROUP BY ISBN;";
