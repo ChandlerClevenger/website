3f4251e06e24129cb0062320d2767cb3c97f3c38
Chandler Clevenger <Chandler888@icloud.com>
2022-03-29 22:52:05 -0400

    add checkout functionality and styling

--- profile/normalUser.php
+++ profile/normalUser.php
@@ -1,28 +1,52 @@
 <?php
 include_once("../connection.php");
+try {
+    $query = "SELECT * FROM BOOK";
+    $sql = $conn->prepare($query);
+    $sql->execute();
+    $result = $sql->fetchAll();
+} catch (exception $e) {
+    echo $e. "<br>ERROR FETCHING DATA!";
+}
 
-$query = "SELECT * FROM BOOK";
-$sql = $conn->prepare($query);
-$sql->execute();
-$result = $sql->fetchAll();
-
-echo "<table>";
+echo "<table id='books'>";
 echo 
-"<tr>
+"<thead><tr>
 <th>ISBN-13</th>
 <th>Name</th>
 <th>Price</th>
 <th>Author</th>
 <th>Image</th>
-</tr>";
-
-foreach($result as $r) {
-    echo "<tr data-id='". $r["ISBN"] ."'>";
-    echo "<td>". $r["ISBN"] . "</td>";
-    echo "<td>". $r["Name"] . "</td>";
-    echo "<td>". "$" . $r["Price"] . "</td>";
-    echo "<td>". $r["Author"] . "</td>";
-    echo "<td>". "<img src='..\images\\". $r["ImgLocation"]."'>" . "</td>";
-    echo "</tr>";
+</tr></thead>";
+echo "<tbody>";
+if(isset($result)) {
+    foreach($result as $r) {
+        echo "<tr data-id='". $r["ISBN"] ."'>";
+        echo "<td>". $r["ISBN"] . "</td>";
+        echo "<td>". $r["Name"] . "</td>";
+        echo "<td>". "$" . $r["Price"] . "</td>";
+        echo "<td>". $r["Author"] . "</td>";
+        echo "<td>". "<img src='..\images\\". $r["ImgLocation"]."'>" . "</td>";
+        echo "</tr>";
+    }
+} else {
+    echo "<tr><td>error</td><td>retreiving</td><td>data</td><td>!!!!!</td><td>!!!!!</td></tr>";
 }
-echo "</table>";
+
+echo "</tbody></table>";
+?>
+
+<div id="actions">
+    <h2>Actions<h2>
+    
+    <button id="checkout" class="button">Checkout</button>
+    <?php
+        // DISPLAY CART
+        try {
+            include("../view/cartTable.php");
+        } catch (exception $e) {
+            echo $e. "FAILURE TO FETCH CART";
+        }
+    ?>
+    <div id="arrow" class="arrows prev"></div>
+</div>
--- profile/script.js
+++ profile/script.js
@@ -1,3 +1,4 @@
+// give rows click functionality
 const rows = document.querySelectorAll("tr");
 rows.forEach((tr) => {
   let id = tr.getAttribute("data-id");
@@ -6,4 +7,28 @@ rows.forEach((tr) => {
       window.location.href = "../itemPage/index.php?id=" + id;
     });
   }
+});
+
+// slide animation
+const arrowDiv = document.querySelector("#arrow");
+arrowDiv.addEventListener("click", (event) => {
+  let actionsDiv = document.querySelector("#actions");
+  let direction = event.target.classList.contains("next") ? "prev" : "next";
+  if (direction == "next") {
+    actionsDiv.style.animationName = "slidein";
+    console.log();
+    event.target.classList.remove("prev");
+    event.target.classList.add("next");
+  } else {
+    actionsDiv.style.animationName = "slideout";
+    event.target.classList.remove("next");
+    event.target.classList.add("prev");
+  }
+});
+
+// checkout button functionality
+const checkout = document.querySelector("#checkout");
+checkout.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.location.href = "../cart";
 });
