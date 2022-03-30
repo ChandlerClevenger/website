302cb48064cbaf2b6ef6efa783c6ca61794327ed
Chandler Clevenger <Chandler888@icloud.com>
2022-03-30 17:50:32 -0400

    add js link

--- cart/index.php
+++ cart/index.php
@@ -35,6 +35,7 @@ $result = $sql->fetchAll();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js" defer></script>
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="../favicon.ico?" type="image/x-icon"/>
     <title>Cart</title>
