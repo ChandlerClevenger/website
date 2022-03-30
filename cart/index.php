647a0f9a3b98bb7b412d2eeef1d2f52aa7e69c7a
Chandler Clevenger <Chandler888@icloud.com>
2022-03-29 21:43:34 -0400

    create cart view

--- cart/index.php
+++ cart/index.php
@@ -41,33 +41,7 @@ $result = $sql->fetchAll();
 </head>
 <body>
     <?php 
-        echo "<table>";
-        echo 
-        "<tr>
-        <th>ISBN-13</th>
-        <th>Book Name</th>
-        <th>Quantity</th>
-        <th>Price</th>
-        <th>Image</th>
-        </tr>";
-        
-        foreach($result as $r) {
-            $ISBN = $r["ISBN"];
-            $query = "SELECT * FROM BOOK WHERE ISBN=:ISBN";
-            $sql = $conn->prepare($query);
-            $sql->execute(["ISBN"=>$ISBN]);
-            $book = $sql->fetch();
-
-            echo "<tr>";
-            echo "<td>". $r["ISBN"] . "</td>";
-            echo "<td>". $book["Name"] . "</td>";
-            echo "<td>". $r["Quantity"] . "</td>";
-            $price = $book["Price"] * $r["Quantity"];
-            echo "<td>$".  number_format($price, 2)  . "</td>";
-            echo "<td>". "<img src='..\images\\". $book["ImgLocation"] ."'>". "</td>";
-            echo "</tr>";
-        }
-        echo "</table>";
+        include("../view/cartTable.php")
     ?>
 </body>
 </html>
---
+++ view/cartTable.php
@@ -0,0 +1,33 @@
+<?php
+$query = "SELECT * FROM CART WHERE Username=:u";
+$sql = $conn->prepare($query);
+$sql->execute(["u"=>$_SESSION['user']]);
+$result = $sql->fetchAll();
+
+echo "<table id='cart'>";
+echo 
+"<tr>
+<th>ISBN-13</th>
+<th>Book Name</th>
+<th>Quantity</th>
+<th>Price</th>
+<th>Image</th>
+</tr>";
+
+foreach($result as $r) {
+    $ISBN = $r["ISBN"];
+    $query = "SELECT * FROM BOOK WHERE ISBN=:ISBN";
+    $sql = $conn->prepare($query);
+    $sql->execute(["ISBN"=>$ISBN]);
+    $book = $sql->fetch();
+
+    echo "<tr>";
+    echo "<td>". $r["ISBN"] . "</td>";
+    echo "<td>". $book["Name"] . "</td>";
+    echo "<td>". $r["Quantity"] . "</td>";
+    $price = $book["Price"] * $r["Quantity"];
+    echo "<td>$".  number_format($price, 2)  . "</td>";
+    echo "<td>". "<img src='..\images\\". $book["ImgLocation"] ."'>". "</td>";
+    echo "</tr>";
+}
+echo "</table>";
