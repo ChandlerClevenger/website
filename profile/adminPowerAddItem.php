acf9ecb69fb72441d4fcd6d7aeea9515e0045622
Chandler Clevenger <Chandler888@icloud.com>
2022-03-30 21:16:39 -0400

    turn echos into html

--- profile/adminPowerAddItem.php
+++ profile/adminPowerAddItem.php
@@ -1,12 +1,18 @@
+<form action="./insertBook.php" method="POST" enctype="multipart/form-data">
+  <label for="ISBN">ISBN-13: </label
+  ><input type="text" name="ISBN" id="ISBN" max="13" />
+  <label for="name">Name: </label
+  ><input type="text" name="name" id="name" max="70" />
+  <label for="price">Price: </label
+  ><input type="number" name="price" id="price" step="0.01" max="99999999" />
+  <label for="author">Author: </label
+  ><input type="text" name="author" id="author" max="70" />
+  <label for="picture">Picture: </label
+  ><input type="file" accept="image/*" name="picture" id="picture" />
+  <input type="submit" name="submit" value="Add Book" />
+</form>
+
 <?php
-echo "<form action='./insertbook.php' method='POST' enctype='multipart/form-data'>";
-echo "<label for='ISBN'>ISBN-13: </label><input type='text' name='ISBN' id='ISBN' max=13>";
-echo "<label for='name'>Name: </label><input type='text' name='name' id='name' max=70>";
-echo "<label for='price'>Price: </label><input type='number' name='price' id='price' step=0.01 max=99999999>";
-echo "<label for='author'>Author: </label><input type='text' name='author' id='author' max=70>";
-echo "<label for='picture'>Picture: </label><input type='file' accept='image/*' name='picture' id='picture'>";
-echo "<input type='submit' name='submit' value='Add Book'>";
-echo "</form>";
 echo "<div id='error'>";
 if(isset($_SESSION['error'])) {
     echo implode("", $_SESSION['error']);
